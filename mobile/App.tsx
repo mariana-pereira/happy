ffd5863bfb5eaf866eb63ecd6b7ce63f3237629b
Mariana Pereira <marianna.22sp@gmail.com>
2020-10-16 13:09:48 -0300

    Day 5 finished

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useFonts } from 'expo-font';
-import { Nunito_600SemiBold_Italic, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Nunito_600SemiBold_Italic, 
+    Nunito_600SemiBold, 
     Nunito_700Bold, 
     Nunito_800ExtraBold
   });
